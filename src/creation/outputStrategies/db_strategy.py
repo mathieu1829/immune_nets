63fc5d35e66f747390863e79c1b5d33be9f3d415
psmigielski <walek.smigielski@gmail.com>
2023-09-14 01:38:21 +0200

    refactor db_stategy

--- src/creation/outputStrategies/db_strategy.py
+++ src/creation/outputStrategies/db_strategy.py
@@ -1,7 +1,4 @@
-import pandas as pd
 import psycopg2
-import uuid
-import numpy as np
 from src.creation.globalSettings import *
 
 def db_strategy(self,algo, user = globalSettings.defaultDBUser, password= globalSettings.defaultDBPassword , host = globalSettings.defaultDBHost, database = globalSettings.defaultDB, **kwargs ):
@@ -9,20 +6,14 @@ def db_strategy(self,algo, user = globalSettings.defaultDBUser, password= global
     conn = psycopg2.connect(user=user, password=password, host=host, database=database, port = "5432")
     cur = conn.cursor()
     cur.execute(f'''CREATE TABLE IF NOT EXISTS {df.name} 
-      (ID INT PRIMARY KEY     NOT NULL,
+      (ID BIGSERIAL PRIMARY KEY     NOT NULL,
       r1        INT    NOT NULL,
       r2        INT    NOT NULL,
-      uuid      CHAR(32)    NOT NULL);''')
+      uuid      CHAR(36) NOT NULL);''')
     conn.commit()
-    nextUUID = uuid.uuid4().hex 
     cur.execute(f"SELECT uuid FROM {df.name}");
-    UUIDS = np.unique(np.array([x[0] for x in cur.fetchall()]))
-    nextID = cur.rowcount 
-    while(len(UUIDS) and (UUIDS == nextUUID).any()):
-        nextUUID = uuid.uuid4().hex
     for idx in df.index:
-        nextID+=1
-        cur.execute(f'INSERT INTO {df.name}(ID,r1,r2,uuid) VALUES ({nextID},{df["r1"][idx]},{df["r2"][idx]},\'{nextUUID}\')')
+        cur.execute(f'INSERT INTO {df.name}(r1,r2,uuid) VALUES ({df["r1"][idx]},{df["r2"][idx]},gen_random_uuid())')
         conn.commit()
 
     conn.close()
