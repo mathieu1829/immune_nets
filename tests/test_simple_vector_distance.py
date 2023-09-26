4356b85b9730292eddd30eb726c2c90aaa72f9d6
Myc0plasmus <jablonski.natan@gmail.com>
2023-09-26 23:14:57 +0200

    simple_vector_distance done

--- tests/test_simple_vector_distance.py
+++ tests/test_simple_vector_distance.py
@@ -21,26 +21,28 @@ class TestSimpleVectorDistance(unittest.TestCase):
 
 
     def test_simple_vector_network(self):
-        for dist in ["BLOSUM62"]: #makeEnumDict(Matrices):
+        for dist in makeEnumDict(Matrices):
             df_net = self.simple_vector.createGraph(clonotypes=self.df, matrix=getattr(Matrices,dist))
             expected_df = pd.DataFrame(data={'r1': [16,17], 'r2': [15,16]})
-            # match dist:
-            #     case "PAM250":
-            #         expected_df = pd.read_csv(Path(__file__).parent / "expected/expected_PAM250_vector.csv")[["r1","r2"]]
-            #     case "PAM30":
-            #         expected_df = pd.read_csv(Path(__file__).parent / "expected/expected_PAM30_vector.csv")[["r1","r2"]]
-            #     case "PAM70":
-            #         expected_df = pd.read_csv(Path(__file__).parent / "expected/expected_PAM70_vector.csv")[["r1","r2"]]
-            #     case "BLOSUM45":
-            #         expected_df = pd.read_csv(Path(__file__).parent / "expected/expected_BLOSUM45_vector.csv")[["r1","r2"]]
-            #     case "BLOSUM50":
-            #         expected_df = pd.read_csv(Path(__file__).parent / "expected/expected_BLOSUM50_vector.csv")[["r1","r2"]]
-            #     case "BLOSUM62":
-            #         expected_df = pd.read_csv(Path(__file__).parent / "expected/expected_BLOSUM62_vector.csv")[["r1","r2"]]
-            #     case "BLOSUM80":
-            #         expected_df = pd.read_csv(Path(__file__).parent / "expected/expected_BLOSUM80_vector.csv")[["r1","r2"]]
-            #     case _:
-            #         expected_df = pd.DataFrame(data={'r1': [16,17], 'r2': [15,16]})
+            match dist:
+                case "PAM250":
+                    expected_df = pd.read_csv(Path(__file__).parent / "expected/expected_PAM250_vector.csv")[["r1","r2"]]
+                case "PAM30":
+                    expected_df = pd.read_csv(Path(__file__).parent / "expected/expected_PAM30_vector.csv")[["r1","r2"]]
+                case "PAM70":
+                    expected_df = pd.read_csv(Path(__file__).parent / "expected/expected_PAM70_vector.csv")[["r1","r2"]]
+                case "BLOSUM45":
+                    expected_df = pd.read_csv(Path(__file__).parent / "expected/expected_BLOSUM45_vector.csv")[["r1","r2"]]
+                case "BLOSUM50":
+                    expected_df = pd.read_csv(Path(__file__).parent / "expected/expected_BLOSUM50_vector.csv")[["r1","r2"]]
+                case "BLOSUM62":
+                    expected_df = pd.read_csv(Path(__file__).parent / "expected/expected_BLOSUM62_vector.csv")[["r1","r2"]]
+                case "BLOSUM80":
+                    expected_df = pd.read_csv(Path(__file__).parent / "expected/expected_BLOSUM80_vector.csv")[["r1","r2"]]
+                case "BLOSUM90":
+                    expected_df = pd.read_csv(Path(__file__).parent / "expected/expected_BLOSUM90_vector.csv")[["r1","r2"]]
+                case _:
+                    expected_df = pd.DataFrame(data={'r1': [16,17], 'r2': [15,16]})
             if not df_net.equals(expected_df):
                 print(f"Error occured while creating matrix: {dist}\n")
                 print("Corrupted net:")
