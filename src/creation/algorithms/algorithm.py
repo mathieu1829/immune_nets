cc4933d37f28d0dd08c708b4ffcdc5741fda68aa
Myc0plasmus <jablonski.natan@gmail.com>
2023-11-17 12:57:08 +0100

    thrown out algorithm class and turned it into a decorator

--- src/creation/algorithms/algorithm.py
+++ src/creation/algorithms/algorithm.py
@@ -1,26 +1,14 @@
 from abc import ABC,abstractmethod
 import pandas as pd
 
-class algorithm(ABC):
-
-    def __init__(self, strategy, **kwargs):
-        self.strategy = strategy
-        self.kwargs = kwargs #arguments passed to strategy
 
     #Checks whether creation algorithm is valid
-    def validateCreationAlgorithm(algo):
-        def algorithmValidator(self,**overrideKwargs):
-            assert type(overrideKwargs['clonotypes']) == type(pd.DataFrame()), f"Expected clonotype, got ${type(overrideKwargs['clonotypes'])}"
-            assert hasattr(overrideKwargs['clonotypes'],"name") , f"Clonotype table has no name"
-            #check whether it has both clonotypes and distance fun
-            mergedKwargs = {**self.kwargs,**overrideKwargs}
-            onlyKwargs = {key:self.kwargs[key] for key in self.kwargs if not key in overrideKwargs}
-            finalArgs = { key:mergedKwargs[key] for key in mergedKwargs if not key in onlyKwargs }
-            return self.strategy(**finalArgs,self = self, algo = algo)
-        return algorithmValidator 
+def algorithm(algo):
+    def algorithmValidator(**kwargs):
+        strategy = kwargs['strategy']
+        assert type(kwargs['clonotypes']) == type(pd.DataFrame()), f"Expected clonotype, got ${type(kwargs['clonotypes'])}"
+        assert hasattr(kwargs['clonotypes'],"name") , f"Clonotype table has no name"
+        return strategy(**kwargs, algo = algo)
+    return algorithmValidator 
 
-    @validateCreationAlgorithm
-    @abstractmethod
-    def createGraph(self,**kwargs): #method used to create a graph
-        return self.creationAlgorithm(**kwargs) #implement this to specify algorithm
     
--- src/creation/algorithms/simple_distance.py
+++ src/creation/algorithms/simple_distance.py
@@ -6,29 +6,29 @@ import pandas as pd
 from src.creation.algorithms.common_methods import *
 from src.creation.algorithms.algorithm import *
 
-class simple_distance(algorithm):
-    def creationAlgorithm(self,clonotypes, distanceFun,threshold = 0.8 , **kwargs):
-        tcr_npa = clonotypes[["tcra_aa", "tcrb_aa"]].dropna().to_numpy()
-        dist_al_trcb = np.zeros(np.shape(tcr_npa)[0] * np.shape(tcr_npa)[0]).reshape(np.shape(tcr_npa)[0],
-                                                                                     np.shape(tcr_npa)[0])
-        dist_al_trcb += -1
-        matrix_len = len(dist_al_trcb)
-        for x in range(0, matrix_len):
-            logging.info(str(x) + " out of str " + str(matrix_len) + "rows process in triangular similarity matrix")
-            for y in range(0 + x, matrix_len):
-                dist_al_trcb[x][y] = distanceFun(tcr_npa[x][0], tcr_npa[y][0]) + distanceFun(tcr_npa[x][1], tcr_npa[y][1])
-                dist_al_trcb[y][x] = dist_al_trcb[x][y]
+@algorithm
+def simple_distance(clonotypes, distanceFun,threshold = 0.8 , **kwargs):
+    tcr_npa = clonotypes[["tcra_aa", "tcrb_aa"]].dropna().to_numpy()
+    dist_al_trcb = np.zeros(np.shape(tcr_npa)[0] * np.shape(tcr_npa)[0]).reshape(np.shape(tcr_npa)[0],
+                                                                                 np.shape(tcr_npa)[0])
+    dist_al_trcb += -1
+    matrix_len = len(dist_al_trcb)
+    for x in range(0, matrix_len):
+        logging.info(str(x) + " out of str " + str(matrix_len) + "rows process in triangular similarity matrix")
+        for y in range(0 + x, matrix_len):
+            dist_al_trcb[x][y] = distanceFun(tcr_npa[x][0], tcr_npa[y][0]) + distanceFun(tcr_npa[x][1], tcr_npa[y][1])
+            dist_al_trcb[y][x] = dist_al_trcb[x][y]
 
-        for x in range(0, len(dist_al_trcb)):
-            self_score = dist_al_trcb[x][x]
-            for y in range(0, len(dist_al_trcb)):
-                dist_al_trcb[x][y] /= self_score
+    for x in range(0, len(dist_al_trcb)):
+        self_score = dist_al_trcb[x][x]
+        for y in range(0, len(dist_al_trcb)):
+            dist_al_trcb[x][y] /= self_score
 
-        dist_al_trcb = np.tril(dist_al_trcb, k=-1)
-        matrix_cutoff = np.where(dist_al_trcb > threshold)
+    dist_al_trcb = np.tril(dist_al_trcb, k=-1)
+    matrix_cutoff = np.where(dist_al_trcb > threshold)
 
-        d = {'r1': matrix_cutoff[0], 'r2': matrix_cutoff[1]}
-        df_net = pd.DataFrame(data=d)
-        df_net.name = clonotypes.name
-        return df_net
+    d = {'r1': matrix_cutoff[0], 'r2': matrix_cutoff[1]}
+    df_net = pd.DataFrame(data=d)
+    df_net.name = clonotypes.name
+    return df_net
 
