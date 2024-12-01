98d6c612b33510f2b9b38d7228031b556c6776ab
Myc0plasmus <jablonski.natan@gmail.com>
2024-12-01 13:07:30 +0000

    Updated nix-shell

--- shell.nix
+++ shell.nix
@@ -30,24 +30,46 @@ let
 
     profile = ''
       set -e
-      eval "$(micromamba shell hook --shell=posix)"
       export MAMBA_ROOT_PREFIX=${builtins.getEnv "PWD"}/.mamba
-	 	  
-      micromamba create -q -n immune-nets
+      # Check if the environment already exists
+      if micromamba env list | grep -q "^immune-nets"; then
+        echo "Creating environment 'immune-nets'..."
+        micromamba create -q -y -n immune-nets
+      else
+        echo "Environment 'immune-nets' already exists. Skipping creation."
+      fi
+
+      echo "$MAMBA_ROOT_PREFIX/envs/immune-nets"
+      chmod u+w "$MAMBA_ROOT_PREFIX/envs/immune-nets"
+
+      
+
+
+      # Install packages only if env.yml has been updated
+      if [ ! -f $MAMBA_ROOT_PREFIX/envs/immune-nets/.env-hash ] || ! sha256sum -c $MAMBA_ROOT_PREFIX/envs/immune-nets/.env-hash; then
+        echo "Installing/updating packages from env.yml..."
+        micromamba install -y -n immune-nets -f env.yml -c conda-forge
+        sha256sum env.yml > $MAMBA_ROOT_PREFIX/envs/immune-nets/.env-hash
+      else
+        echo "Packages already installed. Skipping."
+      fi
+      
+      echo activating the environment ...
+      # Activate the environment
+      eval "$(micromamba shell hook --shell=posix)"
       micromamba activate immune-nets
-	  micromamba install --yes -f env.yml -c conda-forge
 
-	  echo "Using ${pkgs.postgresql_15.name}."
+      echo "Using ${pkgs.postgresql_15.name}."
 
-	  # Setup: other env variables
-	  export SRA=${CRSRA}
-	  export PGPORT="${PORT}"
-	  export PGHOST="${PGDATA}"
-	  export PGDATA="${PGDATA}"
-	  [ ! -d ${PGDATA} ] && pg_ctl initdb -o "-U postgres --no-locale" && cat "${postgresConf}" >> ${PGDATA}/postgresql.conf
-	  pg_ctl -o "-p $PGPORT -k ${PGDATA}" start
-	  micromamba run scripts/envrc
-	  echo all done
+      # Setup: other env variables
+      export SRA=${CRSRA}
+      export PGPORT="${PORT}"
+      export PGHOST="${PGDATA}"
+      export PGDATA="${PGDATA}"
+      [ ! -d ${PGDATA} ] && pg_ctl initdb -o "-U postgres --no-locale" && cat "${postgresConf}" >> ${PGDATA}/postgresql.conf
+      pg_ctl -o "-p $PGPORT -k ${PGDATA}" start
+      micromamba run scripts/envrc
+      echo all done
 
 
       set +e
