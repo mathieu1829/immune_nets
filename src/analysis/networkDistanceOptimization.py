24c3cd7aa50e945abffe95ec7924a336760e5e63
Myc0plasmus <jablonski.natan@gmail.com>
2025-07-20 20:48:05 +0000

    pre-test optuna ready

---
+++ src/analysis/networkDistanceOptimization.py
@@ -0,0 +1,79 @@
+import optuna
+import numpy as np
+from itertools import combinations
+
+from src.analysis.methods.graphlet_composition import graphletComposition
+from src.creation.algorithms.simple_distance import simple_distance
+from src.creation.distance.alignment import sequenceAligner
+from src.creation.distance.levenshtein import levenshteinDistance 
+from scipy.spatial.distance import euclidean
+
+# TO DO - get groups from db
+groups = ["leukemia", "covid", "healthy"]
+
+# TO DO - get repertoires for each group from database
+repertoire_list = [
+        1,
+        2,
+        3,
+        ]
+repertoires = { group:[repertoire_list[i]]  for i,group in enumerate(groups)}
+
+
+def objective(trial):
+    group_results = { group:[] for group in groups}
+    threshold = trial.suggest_float("threshold",low=0.01,high=0.5)
+    distance = trial.suggest_categorical("distance", ["alignment", "levenshtein"])
+    distance_fun = None
+    match distance:
+        case "alignment":
+            substitution_matrix = trial.suggest_categorical("substitution_matrix", [
+                "PAM250",
+                "PAM30",
+                "PAM70",
+                "BLOSUM45",
+                "BLOSUM50",
+                "BLOSUM62",
+                "BLOSUM80",
+                "BLOSUM90"
+                ])
+            distance_fun = sequenceAligner(substitution_matrix) 
+        case "levenshtein":
+            distance_fun = levenshteinDistance()
+
+    for group in groups:
+        for repertoire in repertoires[group]:
+            network = simple_distance(
+                        repertoire=repertoire,
+                        distance=distance_fun,
+                        threshold=threshold
+                    )
+            stats = graphletComposition(network)
+            group_results[group].append(stats.toList())
+                
+    inter_group_distances = []
+    for combo in combinations(groups,2):
+        group_a = group_results[combo[0]]
+        group_b = group_results[combo[1]]
+        inter_group_distance = np.array([euclidean(a,b) for a in group_a for b in group_b ])
+        inter_group_distances.append(inter_group_distance.mean())
+    
+    inter_group_distances = np.array(inter_group_distances)
+    return inter_group_distances.mean() - np.var(inter_group_distances)
+
+if __name__ == '__main__':
+    study = optuna.create_study(direction="maximize")
+    study.optimize(objective, n_trials=20)
+
+    # Best result
+    print("Best score:", study.best_value)
+    print("Best params:", study.best_params)
+
+
+
+
+
+
+
+
+
