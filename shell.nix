4dea959b571398d2cb8a4179e93d2b9335536019
Myc0plasmus <jablonski.natan@gmail.com>
2025-07-24 20:25:03 +0200

    small updates to shell.nix

--- shell.nix
+++ shell.nix
@@ -20,36 +20,36 @@ let
         logging_collector = on
         log_min_error_statement = error
       '';
-  fhs = pkgs.buildFHSEnv {
-    name = "my-fhs-environment";
+   fhs =  pkgs.buildFHSEnv rec{
+     name = "immune-nets";
 
     targetPkgs = _: [
       pkgs.micromamba
-	  pkgs.postgresql_15
+	    pkgs.postgresql_15
     ];
 
     profile = ''
       set -e
       export MAMBA_ROOT_PREFIX=${builtins.getEnv "PWD"}/.mamba
       # Check if the environment already exists
-      if micromamba env list | grep -q "^immune-nets"; then
-        echo "Creating environment 'immune-nets'..."
-        micromamba create -q -y -n immune-nets
+      if micromamba env list | grep -q "^${name}"; then
+        echo "Creating environment '${name}'..."
+        micromamba create -q -y -n ${name}
       else
-        echo "Environment 'immune-nets' already exists. Skipping creation."
+        echo "Environment '${name}' already exists. Skipping creation."
       fi
 
-      echo "$MAMBA_ROOT_PREFIX/envs/immune-nets"
-      chmod u+w "$MAMBA_ROOT_PREFIX/envs/immune-nets"
+      echo "$MAMBA_ROOT_PREFIX/envs/${name}"
+      chmod u+w "$MAMBA_ROOT_PREFIX/envs/${name}"
 
       
 
 
       # Install packages only if env.yml has been updated
-      if [ ! -f $MAMBA_ROOT_PREFIX/envs/immune-nets/.env-hash ] || ! sha256sum -c $MAMBA_ROOT_PREFIX/envs/immune-nets/.env-hash; then
+      if [ ! -f $MAMBA_ROOT_PREFIX/envs/${name}/.env-hash ] || ! sha256sum -c $MAMBA_ROOT_PREFIX/envs/${name}/.env-hash; then
         echo "Installing/updating packages from env.yml..."
-        micromamba install -y -n immune-nets -f env.yml -c conda-forge
-        sha256sum env.yml > $MAMBA_ROOT_PREFIX/envs/immune-nets/.env-hash
+        micromamba install -y -n ${name} -f env.yml -c conda-forge
+        sha256sum env.yml > $MAMBA_ROOT_PREFIX/envs/${name}/.env-hash
       else
         echo "Packages already installed. Skipping."
       fi
@@ -57,7 +57,7 @@ let
       echo activating the environment ...
       # Activate the environment
       eval "$(micromamba shell hook --shell=posix)"
-      micromamba activate immune-nets
+      micromamba activate ${name}
 
       echo "Using ${pkgs.postgresql_15.name}."
 
