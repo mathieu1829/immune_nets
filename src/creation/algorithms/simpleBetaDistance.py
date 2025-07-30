1c6cec45e17b775126c78b8c47dd1ace36a33040
Myc0plasmus <jablonski.natan@gmail.com>
2025-07-30 22:01:10 +0000

    optimized simple distance functions

--- src/creation/algorithms/simpleBetaDistance.py
+++ src/creation/algorithms/simpleBetaDistance.py
@@ -13,25 +13,24 @@ def simpleBetaDistance(repertoire, distance, threshold = 0.8, **kwargs):
     clonotypes = repertoire.clones
     distanceFun = distance.tcr_dist
     tcr_npa = clonotypes[["tcra_aa", "tcrb_aa"]].dropna().to_numpy()
-    dist_al_trcb = np.zeros(np.shape(tcr_npa)[0] * np.shape(tcr_npa)[0]).reshape(np.shape(tcr_npa)[0],
-                                                                                 np.shape(tcr_npa)[0])
+    dist_al_trcb = np.full((np.shape(tcr_npa)[0], np.shape(tcr_npa)[0]),np.inf)
+
     if distance.group == False:
-        dist_al_trcb += -1
         matrix_len = len(dist_al_trcb)
-        for x in range(0, matrix_len):
-            logging.info(str(x) + " out of str " + str(matrix_len) + "rows process in triangular similarity matrix")
-            for y in range(0 + x, matrix_len):
-                dist_al_trcb[x][y] = distanceFun(tcr_npa[x][1], tcr_npa[y][1])
-                dist_al_trcb[y][x] = dist_al_trcb[x][y]
+        for y in range(0, matrix_len):
+            logging.info(str(y) + " out of str " + str(matrix_len) + "rows process in triangular similarity matrix")
+            for x in range(0, y+1):
+                if x != y:
+                    dist_al_trcb[y][x] = distanceFun(tcr_npa[x][1], tcr_npa[y][1])
     else:
         dist_al_trcb = distanceFun(tcr_npa[:,1])
 
     # print(dist_al_trcb)
 
-    dist_al_trcb = np.tril(dist_al_trcb, k=-1)
-    for i in range(len(dist_al_trcb)):
-        for j in range(i,len(dist_al_trcb)):
-            dist_al_trcb[i,j] = float('INF')
+    # dist_al_trcb = np.tril(dist_al_trcb, k=-1)
+    # for i in range(len(dist_al_trcb)):
+    #     for j in range(i,len(dist_al_trcb)):
+    #         dist_al_trcb[i,j] = float('INF')
     matrix_cutoff = np.where(dist_al_trcb < threshold)
     # logging.info(dist_al_trcb)
 
