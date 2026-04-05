820c2c115663dd7f2a3d9ed79d3a3f49d9c17057
Myc0plasmus <jablonski.natan@gmail.com>
2026-04-05 15:57:30 +0200

    added docs to analysis package

---
+++ src/analysis/__init__.py
@@ -0,0 +1,3 @@
+"""
+The analysis package provides methods for analyzing both similarity networks and repertoires. It also includes components related to a heuristic that optimizes the parameters of the network creation algorithm. For example, it contains methods for measuring the distance between two similarity networks, as well as between groups of similarity networks.
+"""
